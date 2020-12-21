fef736964b3a7c5da23f752eca8412f47a142d06
jgh0208 <b99208@naver.com>
2020-12-22 02:35:00 +0900

    1

---
+++ R/first.R
@@ -0,0 +1,25 @@
+A <- sample(1:9,size = 4)
+cond <- c(1,4,5,3)
+A == cond
+if(all(A == cond)){
+  "correct!!"  
+} else if(any(A == cond){
+  ba <- A %in% cond
+  st <- A == cond  
+  if(((A %in% cond) == (A == cond)) == TRUE){
+    N <- as.numeric(((A %in% cond) & (A == cond)) == TRUE)
+    N
+    sum(N)
+    paste0(sum(N),"strike!!")
+  } else if{
+    if(((BA[[1]] == F) == (ST[[1]] == T)){
+      p <- 1
+    } else {
+      p <- 0
+    }
+    p
+    
+    
+  } else {
+    "There is no strike and no ball."
+  }
---
+++ R/수1함.R
@@ -0,0 +1,121 @@
+roll() <- function(cond){
+          A <- sample(1:9,size = 4)
+          assign(A, sample(1:9,size = 4, environment = globalenv())x
+          cond <- sample(1:9,size = 4)
+          if(A == cond)
+}
+
+
+
+A <- sample(1:9,size = 4)
+cond <- c(1,4,5,3)
+
+{if(all(cond == A)){
+    "correct!!"  
+} else if(any(A == cond)){
+    ba <- cond %in% A
+    st <- cond == A  
+    if(ba & st){
+    N <- as.numeric(ba & st)
+    sum(N)
+    v <- paste(sum(N),"strike!!")
+}else {
+      v <- "No Strike."
+}}
+v
+      {ba <- cond %in% A
+      st <- cond == A  
+      BA <- as.list(ba)
+      ST <- as.list(st)
+      if(BA[[1]]){
+        ifelse(ST[[1]]==F,p <- 1,p <- 0)
+      } else {
+        p <- 0
+      }%>%
+      if(BA[[2]]){
+        ifelse(ST[[2]]==F,l <- 1,l <- 0)
+      } else {
+        l <- 0
+      }%>%
+      if(BA[[3]]){
+        ifelse(ST[[3]]==F,o <- 1,o <- 0)
+      } else {
+        o <- 0
+      }%>%
+      if(BA[[4]]){
+        ifelse(ST[[4]]==F,u <- 1,u <- 0)
+      } else {
+        u <- 0
+      }%>%
+      Q <- p+l+o+u%>%
+      Q == 0%>%
+      if(Q == 0){
+        m <- "NO Ball."
+      } else{
+        m <- paste(Q,"Ball!!")
+      }%>%
+     paste(v,m)}}
+
+
+
+
+
+
+
+
+
+
+
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+ba == T     %>%  st == F
+     
+     
+     
+     
+     
+     
+     
+     
+     if(all(A == cond)){
+  "correct!!"  
+} else if(any(A == cond)){
+  ba <- A %in% cond
+  st <- A == cond
+  if((A %in% cond) & (A == cond)){
+    N <- as.numeric(((A %in% cond) & (A == cond)) == TRUE)
+    N
+    if(sum(N) == 0){
+      "No Strike."
+}else {
+   print(paste0(sum(N),"strike!!"))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
